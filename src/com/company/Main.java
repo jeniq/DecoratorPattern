8b1170d881d285ab5c500b46d2cc85cfd3e81b38
Jenia <gr.yevhen@gmail.com>
2016-06-18 13:24:50 +0300

    init

---
+++ src/com/company/Main.java
@@ -0,0 +1,75 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        PrinterInterface printer = new QuotesDecorator(
+                new RightBracketDecorator(
+                        new LeftBracketDecorator(
+                                new Printer("Hello!"))));
+        printer.print();
+    }
+}
+
+interface PrinterInterface{
+    void print();
+}
+
+class Printer implements PrinterInterface{
+    private String value;
+    public Printer(String value) {this.value = value;}
+
+    @Override
+    public void print() {
+        System.out.print(value);
+    }
+}
+
+abstract class Decorator implements PrinterInterface{
+    private PrinterInterface component;
+
+    public Decorator(PrinterInterface component) {
+        this.component = component;
+    }
+
+    public void print() {
+        component.print();
+    }
+}
+
+class QuotesDecorator extends Decorator{
+    public QuotesDecorator(PrinterInterface component){
+        super(component);
+    }
+
+    @Override
+    public void print() {
+        System.out.print("\"");
+        super.print();
+        System.out.print("\"");
+    }
+}
+
+class LeftBracketDecorator extends Decorator{
+    public LeftBracketDecorator(PrinterInterface component){
+        super(component);
+    }
+
+    @Override
+    public void print() {
+        System.out.print("[");
+        super.print();
+    }
+}
+
+class RightBracketDecorator extends Decorator{
+    public RightBracketDecorator(PrinterInterface component){
+        super(component);
+    }
+
+    @Override
+    public void print() {
+        super.print();
+        System.out.print("]");
+    }
+}
